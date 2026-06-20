fcd7a2b20f40969be33a4b11021db5c306349786
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-06-20 15:01:52 +0530

    feat(auth): implement user registration with password hashing and JWT generation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
-import authRouter from './routes/auth.routes';
+import authRouter from './routes/auth.routes.js';
 
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(morgan('dev')) // response time , status code , api-hit , method 
 
 
-app.use("api/auth", authRouter)
+app.use("/api/auth", authRouter)
 
 
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,10 @@
-import userModel from "../models/user.model";
+import userModel from "../models/user.model.js";
+import crypto from 'crypto';
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
 
 
-async function register(req, res){
+export async function register(req, res){
 
     const { username , email , password } = req.body;
 
@@ -17,5 +20,28 @@ async function register(req, res){
             message:"Username or email already exists"
         })
     }
+
+    const hashedPassword = crypto.createHash("sha256").update(password).digest("hex")
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    const token = jwt.sign({
+        id:user._id
+    }, config.JWT_SECRET,{
+        expiresIn: "2d"
+    })
+
+    res.status(201).json({
+        message: "User resgistered successfully",
+        user:{
+            username: user.username,
+            email: user.email
+        }, token
+    })
+
 
 }
